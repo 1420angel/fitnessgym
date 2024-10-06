0f5a4fd67e56f09a163f0faaa3b3b3e4f6b79aa6
1420angel <jansijo5756@gmail.com>
2024-10-07 00:12:26 +0530

    Initial commit

---
+++ 404.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Gym Template">
+    <meta name="keywords" content="Gym, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gym | Template</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/flaticon.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body style="background-image: url(img/hero/hero-3.jpg);">
+
+
+
+
+    <!-- 404 Section Begin -->
+    <section class="section-404">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-404">
+                        <h1>404</h1>
+                        <h3>Opps! This page Could Not Be Found!</h3>
+                        <p>Sorry bit the page you are looking for does not exist, have been removed or name changed</p>
+                      
+                        <a href="./index.php"><i class="fa fa-home"></i> Go back home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- 404 Section End -->
+
+   
+
+   
---
+++ about-us.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Gym Template">
+    <meta name="keywords" content="Gym, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  
+    <title>About Us - Fitness Gym | Your Path to Health and Strength</title>
+    <meta name="description" content="Learn more about Fitness Gym, our mission to promote health and fitness, and how we can help you achieve your fitness goals. Join us and get stronger every day.">
+    <meta name="keywords" content="About Fitness Gym, Health and Fitness, Gym Mission, Fitness Goals, Gym Trainers, Fitness Programs, Personal Training, Gym Services">
+    <meta name="robots" content="index, follow">
+    <link rel="canonical" href="http://www.fitnessgym.com/about">
+
+
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/flaticon.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+     <!-- Header Begin -->
+     <?php
+    include'header.php';
+    ?>
+    <!-- Header End -->
+
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb-bg.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb-text">
+                        <h2>About us</h2>
+                        <div class="bt-option">
+                            <a href="./index.html">Home</a>
+                            <span>About</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section End -->
+
+    <!-- ChoseUs Section Begin -->
+    <?php
+    include'header.php';
+    ?>
+    <!-- ChoseUs Section End -->
+
+    <!-- About US Section Begin -->
+    <section class="aboutus-section">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 p-0">
+                <div class="about-video set-bg" data-setbg="img/about-us.jpg">
+                </div>
+            </div>
+            <div class="col-lg-6 p-0">
+                <div class="about-text">
+                    <div class="section-title">
+                        <span>About Us</span>
+                        <h2>Our Journey to Fitness Excellence</h2>
+                    </div>
+                    <div class="at-desc">
+                        <p>At Fitness Gym, we believe in pushing boundaries and redefining fitness for everyone. Since our founding, we have been dedicated to transforming lives through our tailored training programs, expert coaches, and state-of-the-art facilities. Whether you're just starting your fitness journey or are a seasoned athlete, our team is here to support you every step of the way.</p>
+                        <p>With a strong commitment to health and fitness, we provide a community-driven space where you can achieve your goals, challenge your limits, and become the best version of yourself.</p>
+                    </div>
+                    <div class="about-bar">
+                        <div class="ab-item">
+                            <p>Body Building</p>
+                            <div id="bar1" class="barfiller">
+                                <span class="fill" data-percentage="80"></span>
+                                <div class="tipWrap">
+                                    <span class="tip"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ab-item">
+                            <p>Personal Training</p>
+                            <div id="bar2" class="barfiller">
+                                <span class="fill" data-percentage="85"></span>
+                                <div class="tipWrap">
+                                    <span class="tip"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ab-item">
+                            <p>Fitness Programs</p>
+                            <div id="bar3" class="barfiller">
+                                <span class="fill" data-percentage="75"></span>
+                                <div class="tipWrap">
+                                    <span class="tip"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <!-- About US Section End -->
+
+    <!-- Team Section Begin -->
+    <?php
+    include'team.php';
+    ?>
+    <!-- Team Section End -->
+
+    <!-- Banner Section Begin -->
+    <?php
+    include'banner_section.php';
+    ?>
+    <!-- Banner Section End -->
+
+ 
+
+    <!-- Footer Section Begin -->
+    <?php
+    include'footer.php';
+    ?>
+    <!-- Footer Section End -->
+
+    <!-- Search model Begin -->
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch">+</div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
+        </div>
+    </div>
+    <!-- Search model end -->
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/masonry.pkgd.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+
+
+</body>
+
+</html>
